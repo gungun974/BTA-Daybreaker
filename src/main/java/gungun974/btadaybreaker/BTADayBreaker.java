dc32125d2f52abc943c0ee9f9fbb540a7ea1e62d
Gungun974 <xfelix974@gmail.com>
2026-07-21 00:10:42 +0400

    Update to release 8.0

--- src/main/java/gungun974/btadaybreaker/BTADayBreaker.java
+++ src/main/java/gungun974/btadaybreaker/BTADayBreaker.java
@@ -3,10 +3,11 @@ package gungun974.btadaybreaker;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.HalpLibe;
 
 
 public class BTADayBreaker implements ModInitializer {
-    public static final String MOD_ID = "btadaybreaker";
+    public static final String MOD_ID = HalpLibe.registerMod("btadaybreaker");
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
